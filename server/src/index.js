f92e19c2f2327ab96832253e46c112a26ff66457
Louis Andrew <louisandrew3@gmail.com>
2021-01-02 09:49:21 +0100

    Adding linter.

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,19 @@
-const app = require("express")();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {
+    cors: {
+        // handling CORS -> https://socket.io/docs/v3/handling-cors/
+        origin: 'http://localhost:3000', // Request from this address won't be blocked.
+        methods: ['GET', 'POST'],
+    },
+})
 
-io.on("connection", (socket) => {
-  socket.on("message", (event) => {
-    io.emit("message", event);
-  });
-});
+io.on('connection', (socket) => {
+    socket.on('post', ({ roomNum, ...rest }) => {})
+})
 
-const PORT = 4000;
+const PORT = 4000
 
 http.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
-});
+    console.log('Listening on port ' + PORT)
+})
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
+
+import logo from './logo.svg'
+import './App.css'
+
+const socket = io.connect('http://localhost:4000')
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // connect to socket io here.
+    const [clientNum, setClientNum] = useState(0)
+    const [connected, setConnected] = useState(false)
+
+    useEffect(() => {
+        setClientNum(Math.round(Math.random() * 10))
+    }, [])
+
+    socket.on('connect', () => {
+        if (!connected) {
+            setConnected(true)
+        }
+    })
+
+    socket.on('message', (event) => {
+        if (connected) {
+            console.log(event)
+        }
+    })
+
+    const emitMsg = () => {
+        if (connected) {
+            socket.emit('post', { msg: 'Hello, World!', clientNum, roomNum: 0 })
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <button style={{ marginTop: 16 }} onClick={emitMsg}>
+                    Emit msg.
+                </button>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
